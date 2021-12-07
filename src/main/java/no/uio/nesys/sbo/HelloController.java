c0c569000670b27b8398f6ee46f8d336e6a40114
Shirro <s.roma1997@gmail.com>
2021-12-07 12:05:23 +0300

    3

--- src/main/java/no/uio/nesys/sbo/HelloController.java
+++ src/main/java/no/uio/nesys/sbo/HelloController.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!"; //This string is used in JUnit HelloControllerTest.java
-	}
-	
-	@RequestMapping("hello")
-    public String sayHello(){
-        return ("Hello, SpringBoot on @RequestMapping");
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!"; //This string is used in JUnit HelloControllerTest.java
+    }
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
     }
 
 }
